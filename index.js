92140de6ecbf8234dc24095c689e3599fce7ff78
TanWeeKiat <tanwee.kiat@live.com.sg>
2019-10-25 12:20:33 +0800

    heroko

--- index.js
+++ index.js
@@ -15,13 +15,38 @@ var sha256 = require('js-sha256');
 var SALT = SALTY;
 
 // Initialise postgres client
-const configs = {
-  user: 'tanweekiat',
-  host: '127.0.0.1',
-  database: 'workout_routine',
-  port: 5432,
-};
+const url = require('url');
 
+//check to see if we have this heroku environment variable
+if( process.env.DATABASE_URL ){
+
+  //we need to take apart the url so we can set the appropriate configs
+
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  //make the configs object
+  var configs = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+  };
+
+}else{
+
+  //otherwise we are on the local network
+  var configs = {
+      user: 'tanweekiat',
+      host: '127.0.0.1',
+      database: 'workout_routine',
+      port: 5432
+  };
+}
+
+//this is the same
 const pool = new pg.Pool(configs);
 
 pool.on('error', function (err) {
